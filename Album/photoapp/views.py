53021f683876ad10d62bf3301b26cdda667cf321
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-01-29 19:09:49 +0300

    Update views.py
    updated the views

--- Album/photoapp/views.py
+++ Album/photoapp/views.py
@@ -7,7 +7,7 @@ from django.contrib import messages
 
 # Create your views here.
 def index(request):
-    return render(request,('index.html'))
+    return render(request,('home.html'))
 
 def login_request(request):
     if request.method == 'POST':
