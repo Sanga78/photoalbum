3fbd77e0e43521097b792557f9f0c5347b492775
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-01-30 09:41:59 +0300

    Update urls.py
    updated the urls

--- Album/Album/urls.py
+++ Album/Album/urls.py
@@ -18,10 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic import TemplateView 
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include("photoapp.urls")),
+    path("app/",include("photoapp.urls")),
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('accounts/', include('allauth.urls')), # all OAuth operations will be performed under this route
+    path('logout', LogoutView.as_view()) # default Django logout view at /logout
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
