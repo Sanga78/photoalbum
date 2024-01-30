7aeb026d31ad5bb0b5a37e27791661d5b7683e83
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-01-30 09:43:22 +0300

    Update urls.py
    updated the app urls

--- Album/photoapp/urls.py
+++ Album/photoapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns=[
+    path('index/',views.index,name='index'),
     path('register', views.register, name='user_register'),
     path('',views.login_request,name='login'),
     path('logout/',views.logout_request,name='logout')
