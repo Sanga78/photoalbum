e7935a336511e77d93eb07a25094c24752dd918a
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-01-31 17:59:14 +0300

    Update models.py
    created models

--- Album/photoapp/models.py
+++ Album/photoapp/models.py
@@ -1,16 +1,24 @@
 from django.db import models
+from users.models import User
+from django.utils import timezone
+from django.urls import reverse
 
 # Create your models here.
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    username = models.CharField(max_length=100)
-    email = models.EmailField()
-
 class Album(models.Model):
-    album_title = models.CharField(max_length = 17)
-    user_id = models.ForeignKey(User,on_delete = models.CASCADE)
+    album_title = models.CharField(max_length=100)
+    date_created = models.DateField(default=timezone.now)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.album_title
+    
+    def get_absolute_url(self):
+        return reverse('album-detail', kwargs={'pk': self.pk})
 
 class Photo(models.Model):
+    photo_title = models.CharField(max_length=100)
     album_id = models.ForeignKey(Album,on_delete=models.CASCADE)
-    photo_title = models.CharField(max_length = 25)
     image_url = models.URLField()
+
+    def __str__(self):
+        return self.photo_title
