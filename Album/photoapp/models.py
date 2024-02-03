a501ab7bf6ae8a81d079d6fee5548c2a10439eb1
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-02-03 22:04:04 +0300

    Update models.py
    updated the models

--- Album/photoapp/models.py
+++ Album/photoapp/models.py
@@ -1,24 +1,58 @@
 from django.db import models
-from users.models import User
 from django.utils import timezone
-from django.urls import reverse
+from PIL import Image
 
 # Create your models here.
+            
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
+def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
 class Album(models.Model):
     album_title = models.CharField(max_length=100)
     date_created = models.DateField(default=timezone.now)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.album_title
-    
-    def get_absolute_url(self):
-        return reverse('album-detail', kwargs={'pk': self.pk})
+
 
 class Photo(models.Model):
     photo_title = models.CharField(max_length=100)
     album_id = models.ForeignKey(Album,on_delete=models.CASCADE)
-    image_url = models.URLField()
+    image = models.ImageField(default='default.jpg', upload_to='photos')
 
     def __str__(self):
         return self.photo_title
+    
+def save(self, *args, **kwargs):
+        super(Photo, self).save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
