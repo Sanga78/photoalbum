12b5b7d6b0a28908ff3f2e3c4afe9d56e82264ae
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-02-03 22:04:22 +0300

    Update views.py
    updated the views,used cbv

--- Album/photoapp/views.py
+++ Album/photoapp/views.py
@@ -1,60 +1,127 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth.models import User
+from django.shortcuts import render,get_object_or_404,redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate, logout
-# from .forms import CustomerSignUpForm
+from django.contrib import messages
+from .models import Album,Photo,User
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse,reverse_lazy
+from django.views.generic.edit import FormView
+from .forms import PhotoForm
 from django.contrib import messages
 
 # Create your views here.
-def index(request):
-    return render(request,('home.html'))
+class HomeView(ListView):
+    template_name = 'home.html'
+    model = User
+    context_object_name = 'users'
 
-def login_request(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request=request, data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = authenticate(username=username,password=password)
-            if user is not None:
-                login(request, user)
-                messages.info(request,f"Logged in Successfully as {username}")
-                return redirect('index')
-            else:
-                messages.info(request,"Account doesn't exists")
-                return redirect('register')
-        else:
-            messages.info(request,'Invalid credentials')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'index.html')
+def about(request):
 
-def register(request):
-    if request.method == 'POST':
-        name = request.POST['name']
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password1']
-        password2 = request.POST['password2']
+    return render(request,'about.html', {'title':'about'})
+
+
+class AlbumListView(ListView):
+    model = Album
+    template_name = 'home.html'
+    context_object_name = 'albums'
+    ordering = ['-date_created']
+    paginate_by = 3
+
+class UserAlbumListView(ListView):
+    model = Album
+    template_name = 'user_albums.html'
+    context_object_name = 'albums'
+    paginate_by = 3
     
-        if password == password2:
-            if User.objects.filter(email=email).exists():
-                messages.info(request,'Email Already Exists')
-                return redirect('user_register')
-            elif User.objects.filter(username=username).exists():
-                messages.info(request,'Username Already Exists')
-                return redirect('user_register')
-            else:
-                user = User.objects.create_user(username=username,name=name,email=email,password=password)
-                user.save()
-                return redirect('login')
-        else:
-            messages.info(request,'Password Not The Same')
-            return redirect('user_register')
-    else:
-        return render(request,'user_register.html')
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Album.objects.filter(user=user).order_by('-date_created')
 
-def logout_request(request):
-    logout(request)
-    messages.info(request,"Logged out successfully!")
-    return redirect('/')
+
+class AlbumCreateView(LoginRequiredMixin, CreateView):
+    model = Album
+    fields = ['album_title']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+
+        # Check if an album with the same title already exists
+        existing_album = Album.objects.filter(user=self.request.user, album_title=form.cleaned_data['album_title']).first()
+
+        if existing_album:
+            # Display a message to the user
+            messages.error(self.request, 'An album with this title already exists.')
+            return redirect('album-create')  
+        return super().form_valid(form)
+    def get_success_url(self):
+        return reverse_lazy('album-detail', args=[str(self.object.id)])
+
+class AlbumUpdateView(LoginRequiredMixin, UpdateView):
+    model = Album
+    fields = ['album_title']
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        album = self.get_object()
+        if self.request.user == album.user:
+            return True
+        return False
+            
+
+class AlbumDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Album
+    success_url = '/'
+    def test_func(self):
+        album = self.get_object()
+        if self.request.user == album.user:
+            return True
+        return False
+
+class AlbumDetailView(DetailView):
+    template_name = 'Adetail.html'
+    model = Album
+    context_object_name = 'album'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['album_photos'] = Photo.objects.filter(album_id=self.object)
+        return context
+
+class PhotoDetailView(DetailView):
+    template_name = 'photo_detail.html'
+    model = Photo
+    context_object_name = 'photo'
+    
+
+class AddPhotoView(FormView):
+    template_name = 'add_photo.html'
+    form_class = PhotoForm
+
+    def form_valid(self, form):
+        album_id = self.kwargs['pk']
+        album_id = get_object_or_404(Album, pk=album_id)
+
+        photo_title = form.cleaned_data['photo_title']
+        image = form.cleaned_data['image']
+
+        Photo.objects.create(album_id=album_id, photo_title=photo_title,image=image)
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('album-detail', kwargs={'pk': self.kwargs['pk']})
+class UserDetailView(DetailView):
+    template_name = 'user_detail.html'
+    model = User
+    context_object_name = 'user'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_albums'] = Album.objects.filter(user=self.object)
+        return context
+    
+ 
