908f3604244cf04f85edebbcea2d390f310c9549
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-02-06 22:06:09 +0300

    Update admin.py
    registered the apps

--- Album/photoapp/admin.py
+++ Album/photoapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Album,Photo
+from .models import Album,Photo,Profile
 # Register your models here.
 
 
 admin.site.register(Album)
 admin.site.register(Photo)
+admin.site.register(Profile)
