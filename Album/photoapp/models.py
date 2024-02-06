24a57a2e022b03e908874e318d170f039437e8d2
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-02-06 22:06:32 +0300

    Update models.py
    updated the models

--- Album/photoapp/models.py
+++ Album/photoapp/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 from django.utils import timezone
 from PIL import Image
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 
 # Create your models here.
             
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    username = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
+class User(AbstractUser,PermissionsMixin):
+    fullname = models.CharField(max_length=100)
 
     def __str__(self):
         return self.username
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
