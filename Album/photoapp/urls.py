84fb8ae35fc3501be4e75bd46e5c5fd6e42dfffe
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-02-12 20:22:09 +0300

    Update urls.py
    updated the urls

--- Album/photoapp/urls.py
+++ Album/photoapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
-from .views import HomeView, AlbumDetailView, AlbumCreateView, AlbumUpdateView, AlbumDeleteView, AddPhotoView,UserDetailView,PhotoDetailView
+from .views import HomeView, AlbumDetailView, AlbumCreateView, AlbumUpdateView, AlbumDeleteView, AddPhotoView,UserDetailView,PhotoDetailView,UserAlbumListView
 urlpatterns =[
-    path('', HomeView.as_view(), name='album-home'),
+    path('', views.index, name='index'),
+    path('home', HomeView.as_view(), name='album-home'),
     path('album/<int:pk>/', AlbumDetailView.as_view(), name='album-detail'),
     path('user/<int:pk>/', UserDetailView.as_view(), name='user_detail'),
+    path('user/<str:username>/albums/', UserAlbumListView.as_view(), name='user_albums'),
     path('photo/<int:pk>/', PhotoDetailView.as_view(), name='photo_detail'),
     path('album/new/', AlbumCreateView.as_view(), name='album-create'),
     path('album/<int:pk>/add_photo/', AddPhotoView.as_view(), name='add_photo'),
