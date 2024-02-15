2b6bc3baeab24a4c909f6db1e8b9f55c4ca81127
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-02-15 10:32:34 +0300

    Update views.py
    updated views

--- Album/photoapp/views.py
+++ Album/photoapp/views.py
@@ -165,30 +165,7 @@ def logout_request(request):
     logout(request)
     messages.info(request,"Logged out successfully!")
     return redirect('/')
-@login_required
-# def profile(request):
-#     if request.method == 'POST':
-#         u_form= UserUpdateForm(request.POST, instance=request.user)
-#         p_form= ProfileUpdateForm(request.POST, 
-#                                   request.FILES, 
-#                                   instance=request.user.profile)
-
-#         if u_form.is_valid() and p_form.is_valid():
-#             u_form.save()
-#             p_form.save()
-#             messages.success(request, f'You Account has been Updated')
-#             return redirect('profile')
-#     else:
-#         u_form= UserUpdateForm(instance=request.user)
-#         p_form= ProfileUpdateForm(instance=request.user.profile)
-
-#     context = {
-#         'u_form':u_form,
-#         'p_form':p_form
-#     }
-
-#     return render(request, 'users/profile.html', context)
- 
+@login_required 
 def profile(request):
     user_profile = Profile.objects.get_or_create(user=request.user)[0]
 
