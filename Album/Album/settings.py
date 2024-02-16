a72be1592661d9fc5a60a29e75d1df581472523d
kelvin sanga <kelvinkipkosgeisanga@gmail.com>
2024-02-16 15:41:58 +0300

    Update settings.py
    updated settings

--- Album/Album/settings.py
+++ Album/Album/settings.py
@@ -164,16 +164,15 @@ SOCIALACCOUNT_PROVIDERS = {
 SITE_ID = 2
 
 LOGIN_REDIRECT_URL = 'album-home'
+LOGOUT_REDIRECT_URL = '/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 EMAIL_BACKEND= 'django.core.mail.backend.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_PORT = 25
-EMAIL_USE_TLS = True
 
 AUTH_USER_MODEL = 'photoapp.User'
